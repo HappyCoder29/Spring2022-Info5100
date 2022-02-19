44aacafd8b7254c3f9d29add327337d4a5573221
Ashish <ashish@drillmaps.com>
2022-02-19 15:20:16 -0800

    Checking in code for Multiton Design Pattern

--- DesignPatterns/MultitonDesignpattern/src/edu/northeastern/ashish/SecurityCamera.java
+++ DesignPatterns/MultitonDesignpattern/src/edu/northeastern/ashish/SecurityCamera.java
@@ -1,7 +1,36 @@
 package edu.northeastern.ashish;
 
-public class SecurityCamera {
+import java.util.HashMap;
 
+/*
+* 1. Create a class
+* 2. Create a private static Hashmap for the class with ID, Class
+* 3. Create static private object for locking
+* 4. Create a public method which will return the Class instance for particular ID
+* 5. If the ID is not initialized, double lock and create a new instance and add it in the Hashmap
+* 6. If the ID exists return the value from hashmap
+*
+* */
+
+public class SecurityCamera {
+    private static HashMap<Integer, SecurityCamera> cameras = new HashMap<>();
+
+    private SecurityCamera(){}
+
+    private static Object obj = new Object();
+
+    public static SecurityCamera getCamera( int id){
+
+        if( !cameras.containsKey(id) ){
+            synchronized (obj){
+                if( !cameras.containsKey(id) ){
+                    cameras.put(id, new SecurityCamera());
+                }
+            }
+        }
+        return cameras.get(id);
+
+    }
 
 
 }
